fccf98f0fe70e44c191ebd1261dd111f5e1086aa
Varun <Varun@Varuns-MacBook-Pro.local>
2023-09-30 16:07:25 +0530

    Added google auth, clientID needs to be set

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportGoogle = require('./config/passport-google-strategy');
 const MongoStore = require('connect-mongo');
 
 const flash = require('connect-flash');
