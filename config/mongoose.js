e6fa1b1a53ef5effa046b9074d68199a689d549b
gaddevarunteja <gaddevarunteja@gmail.com>
2023-10-01 18:41:20 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/auth-db');
+mongoose.connect('mongodb://0.0.0.0/auth-db');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error in connecting to db'));
