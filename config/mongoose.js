50a106d2ba53c9f39f5b7eb40cca757e863fc996
Varun <Varun@Varuns-MacBook-Pro.local>
2023-10-04 13:45:48 +0530

    Added MONGODB_URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0/auth-db');
+console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error in connecting to db'));
